9a9c5ad658a19474c5a294ef97f889adbecf615e
Diana Ramirez <37516634+dianaramirez16@users.noreply.github.com>
2020-11-18 01:25:21 -0800

    more inline styles

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 10}}>
-      <View>
-        <TextInput />
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItem: 'center'}}>
+        <TextInput placeholder="Course Goal" style={{width:'80%', borderBottomColor: 'black', borderBottomWidth:1, padding:10 }}/>
         <Button title="ADD" />
         
       </View>
@@ -16,7 +16,7 @@ export default function App() {
     </View>
   );
 }
-//comments
+
 const styles = StyleSheet.create({
   
 });
