cf52295842126804a285c23186df26f4303381ed
Diana Ramirez <37516634+dianaramirez16@users.noreply.github.com>
2020-11-24 01:13:09 -0800

    setting up flexbox lesson

--- App.js
+++ App.js
@@ -4,14 +4,39 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItem: 'center'}}>
-        <TextInput placeholder="Course Goal" style={{width:'80%', borderBottomColor: 'black', borderBottomWidth:1, padding:10 }}/>
-        <Button title="ADD" />
-        
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View>
-
+      <View
+        style={{
+          backgroundColor: 'blue',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
       </View>
     </View>
   );
