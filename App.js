32bcefdd3cab54267aad09d8643be71386ed9b7b
Diana Ramirez <37516634+dianaramirez16@users.noreply.github.com>
2020-11-25 00:19:10 -0800

    switching back to text input to use StyleSheet object

--- App.js
+++ App.js
@@ -4,46 +4,31 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ 
-      padding: 50, 
-      flexDirection: "row", 
-      width:'80%',
-      height: 300,
-      justifyContent:'space-around',
-      alignItems:'center'}}>
-      <View
-        style={{
-          backgroundColor: 'red',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'blue', 
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-         
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+        />
+        <Button title="ADD" />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: { 
+    padding: 50, 
+    flexDirection: "row", 
+    width:'80%',
+    height: 300,
+    justifyContent:'space-around',
+    alignItems:'center'
+  },
+  inputContainer: {
+    backgroundColor: 'red',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
